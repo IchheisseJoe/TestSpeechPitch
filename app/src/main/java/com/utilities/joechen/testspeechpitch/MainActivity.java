f3cfe3b3640df8cbde718fa2f4d1aaff55078b14
Chen Chao Yi <ichheissejoe@gmail.com>
2017-02-17 20:24:42 +0800

    update for new function

--- app/src/main/java/com/utilities/joechen/testspeechpitch/MainActivity.java
+++ app/src/main/java/com/utilities/joechen/testspeechpitch/MainActivity.java
@@ -9,15 +9,32 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.util.Locale;
+import java.util.Random;
+import java.lang.System;
 
 public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener,
 	SeekBar.OnSeekBarChangeListener, Button.OnClickListener
 {
 	private static final String			TAG = "TestSpeechPitch";
-	private static final String			CH  = "現在正在測試語音的語調";
-	private static final String			EN  = "I try to say something, OK?";
+	private static final String[]		CH  =
+		{
+			"現在正在測試語音的語調",
+			"今天天氣不錯，適合出去走走。",
+			"我不想再這樣搞下去了！",
+			"公司什麼時候幫我加薪啊？",
+			"機器人是未來的明星產業。"
+		};
+	private static final String[]			EN  =
+		{
+			"I am trying to say something now.",
+			"I would like to ask you do not torture me.",
+			"The news conference was not scheduled until Thursday morning.",
+			"Please stop talking and pay attention to work.",
+			"Just to be the next to be with you"
+		};
 	private TextToSpeech				m_TTS;
 	private EditText					m_etPitchValue;
 	private SeekBar						m_sbPitchValue;
@@ -39,15 +56,12 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
 		m_sbPitchValue.setProgress(50);
 		m_btnSayChinese.setOnClickListener(this);
 		m_btnSayEnglish.setOnClickListener(this);
-		/*
-		if(m_TTS.isLanguageAvailable(Locale.CHINESE) < 0 ||
-			m_TTS.isLanguageAvailable(Locale.TAIWAN) < 0 ||
-			m_TTS.isLanguageAvailable(Locale.TRADITIONAL_CHINESE) < 0)
-		{
-			m_btnSayChinese.setVisibility(View.INVISIBLE);
-			Log.d(TAG, "Does not support Chinese voice");
-		}
-		*/
+		//if(m_TTS.isLanguageAvailable(Locale.TAIWAN)==TextToSpeech.LANG_NOT_SUPPORTED)
+		//{
+		//	m_btnSayChinese.setVisibility(View.INVISIBLE);
+		//	Toast.makeText(getApplicationContext(), R.string.no_zh_tw, Toast.LENGTH_LONG);
+		//	Log.d(TAG, "Does not support Chinese voice");
+		//}
     }
 
 	@Override
@@ -60,7 +74,8 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
 	public void onProgressChanged(SeekBar seekBar, int progress_value, boolean b)
 	{
 		// Log.d(TAG, "i = " + i);
-		float value = (float)progress_value / 50.0f;
+		int pv= progress_value == 0 ? 1:progress_value;
+		float value = (float)pv / 50.0f;
 		String str=String.valueOf(value);
 		m_etPitchValue.setText(str);
 	}
@@ -82,28 +97,34 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
 	{
 		if(view.getId() == R.id.buttonSayChinese)
 		{
+			Random rand = new Random();
+			rand.setSeed(System.currentTimeMillis());
+			int value = rand.nextInt(CH.length);
 			m_TTS.setLanguage(Locale.TAIWAN);
 			m_TTS.setPitch(Float.parseFloat(m_etPitchValue.getText().toString()));
 			if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
 			{
-				m_TTS.speak(CH, TextToSpeech.QUEUE_FLUSH, null, null);
+				m_TTS.speak(CH[value], TextToSpeech.QUEUE_FLUSH, null, null);
 			}
 			else
 			{
-				m_TTS.speak(CH, TextToSpeech.QUEUE_FLUSH, null);
+				m_TTS.speak(CH[value], TextToSpeech.QUEUE_FLUSH, null);
 			}
 		}
 		else if(view.getId() == R.id.buttonSayEnglish)
 		{
+			Random rand = new Random();
+			rand.setSeed(System.currentTimeMillis());
+			int value = rand.nextInt(EN.length);
 			m_TTS.setLanguage(Locale.US);
 			m_TTS.setPitch(Float.parseFloat(m_etPitchValue.getText().toString()));
 			if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
 			{
-				m_TTS.speak(EN, TextToSpeech.QUEUE_FLUSH, null, null);
+				m_TTS.speak(EN[value], TextToSpeech.QUEUE_FLUSH, null, null);
 			}
 			else
 			{
-				m_TTS.speak(EN, TextToSpeech.QUEUE_FLUSH, null);
+				m_TTS.speak(EN[value], TextToSpeech.QUEUE_FLUSH, null);
 			}
 		}
 	}
